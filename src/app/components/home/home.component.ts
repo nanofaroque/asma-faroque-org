ae41c3920446454dfa879a88ccf8f4f8d394ae7e
Miraje <mdalamin8521@gmail.com>
2018-10-18 21:28:08 +0600

    content dynamic from component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,6 +15,38 @@ export class HomeComponent implements OnInit {
     {img: 'assets/images/slide4.png', alt: '',  text: 'Together we can build the Earth'},
     {img: 'assets/images/slide5.png', alt: '', text: 'Together we can build the Earth'}
   ];
+
+
+
+  public bannerText = {
+    title:"EVERY CHILD SHOULD GROW UP IN A BETTER WORLD",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been\n" +
+    "              the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley\n" +
+    "              of type and scrambled it to make a type specimen book. It has survived not only five centuries,\n" +
+    "              but also the leap into electronic typesetting, remaining"
+  };
+
+  public sidebarContent = {
+    leftSidebar:{
+      video:'assets/educationalvideo.mp4'
+    },
+    rightSidebar:{
+      title:'Learn more about out mission and our commitment to a better future for our children '
+    }
+  };
+
+
+  public quotes = {
+    title: `“The only way that we reach out full human potential is if we’re able to unlock the gifts of
+              every person around the world.”`,
+    author:`-Omar Faroque and Asma Omar`
+  };
+
+  public contact = {
+    title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been\n" +
+    "              the industry's standard dummy text ever since the 1500s,"
+  };
+
   constructor(config: NgbCarouselConfig) {
     config.showNavigationArrows = true;
     config.showNavigationIndicators = true;
@@ -26,4 +58,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
 }
