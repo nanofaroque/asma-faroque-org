7548ce146910f7a713d9bae2dc91257a932a0ea3
Miraje <mdalamin8521@gmail.com>
2018-10-18 18:49:55 +0600

    redesign with bootstrap

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,10 +10,10 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 export class HomeComponent implements OnInit {
   public sliderArray = [
     {img: 'assets/images/slide1.png', alt: '', text: 'Together we can build the Earth'},
-    {img: 'assets/images/slide2.jpg', alt: '',  text: 'Together we can build the Earth'},
-    {img: 'assets/images/slide3.jpg', alt: '', text: 'Together we can build the Earth'},
-    {img: 'assets/images/slide4.jpg', alt: '',  text: 'Together we can build the Earth'},
-    {img: 'assets/images/slider5.jpeg', alt: '', text: 'Together we can build the Earth'}
+    {img: 'assets/images/slide2.png', alt: '',  text: 'Together we can build the Earth'},
+    {img: 'assets/images/slide3.png', alt: '', text: 'Together we can build the Earth'},
+    {img: 'assets/images/slide4.png', alt: '',  text: 'Together we can build the Earth'},
+    {img: 'assets/images/slide5.png', alt: '', text: 'Together we can build the Earth'}
   ];
   constructor(config: NgbCarouselConfig) {
     config.showNavigationArrows = true;
